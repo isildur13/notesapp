94f319feac4f7cdf042d81944cfa4a1edf6ae499
isildur13 <panchalyash13@gmail.com>
2020-04-21 18:40:29 +0530

    Basic functions defined..

--- src/functions.c
+++ src/functions.c
@@ -1,6 +1,38 @@
 #include<stdio.h>
+#include<mysql/mysql.h>
+
+
+//-----------------Database related functions.
+// Core DB tasks.
+db_connection_status(){}
+db_connection_start(){}
+db_connection_stop(){}
+
+
+db_create_table(){}
+db_create_db(){}
+db_delete_table(){}
+db_delete_db(){}
+
+
+// DB functions.
+db_create_entry(){}
+db_update_entry(){}
+db_delete_entry(){}
+db_view_entry(){}
+
+
+
+//----------------Notesapp Core functions.
 
 create_note(){}
 edit_note(){}
 delete_note(){}
 view_note(){}
+
+
+//Main Menu function
+
+main_menu(){}
+
+
